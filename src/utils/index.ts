c3ff2ce7c90d0a1c12ff1833b7339351305eeb34
Milon <milonkremer@gmail.com>
2020-09-19 12:58:25 +0200

    updated phoen input

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,7 +3,7 @@ import dialCodes from '../assets/dialCodes'
 export function normalize(phoneNumber: string): string {
     phoneNumber = phoneNumber.replace(/[()]/g, '').trim() // removes "(" and ")" and spaces
     if (phoneNumber.length >= 2) {
-        let number = phoneNumber.replace(/^00/, '+')
+        const number = phoneNumber.replace(/^00/, '+')
         const dialCode = findDialCode(number)
         if (dialCode) {
             let x = number.replace(dialCode.dialCode, '').replace(/^0+/, '')
@@ -14,8 +14,7 @@ export function normalize(phoneNumber: string): string {
 }
 
 export function findDialCode(phoneNumber: string): any {
-    let dialCode = undefined
-    dialCode = dialCodes.find(x => x.dialCode === phoneNumber.substr(0, 5))
+    let dialCode = dialCodes.find(x => x.dialCode === phoneNumber.substr(0, 5))
     if (!dialCode) dialCode = dialCodes.find(x => x.dialCode === phoneNumber.substr(0, 4))
     if (!dialCode) dialCode = dialCodes.find(x => x.dialCode === phoneNumber.substr(0, 3))
     if (!dialCode) dialCode = dialCodes.find(x => x.dialCode === phoneNumber.substr(0, 2))
